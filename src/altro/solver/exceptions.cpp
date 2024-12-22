cf03e2d86871eb2b98f58981c869176999ea5220
Kaiserreich-Unofficial <2313594637@qq.com>
2024-12-22 23:50:02 +0800

    恢复了exceptions的输出方式

--- src/altro/solver/exceptions.cpp
+++ src/altro/solver/exceptions.cpp
@@ -99,11 +99,7 @@ namespace altro
 
   void PrintErrorCode(ErrorCodes err)
   {
-    // fmt::print("Got error code {}: {}\n", static_cast<int>(err), ErrorCodeToString(err));
-    Serial.print("Got error code ");
-    Serial.print(static_cast<int>(err));
-    Serial.print(": ");
-    Serial.println(ErrorCodeToString(err));
+    fmt::print("Got error code {}: {}\n", static_cast<int>(err), ErrorCodeToString(err));
   }
 
 } // namespace altro
--- src/altro/solver/exceptions.hpp
+++ src/altro/solver/exceptions.hpp
@@ -17,8 +17,7 @@
   (throw(AltroErrorException((msg), code)), code)
 #else
 #define ALTRO_THROW(msg, code) \
-  // (fmt::print(fmt::fg(fmt::color::red), "ALTRO ERROR Code {}: {} {}:{}\n  Message: {}\n", (static_cast<int>(code)), ErrorCodeToString(code), __FILE__, __LINE__, (msg)), code)
-  (Serial.print("ALTRO ERROR Code "); Serial.print(static_cast<int>(code)); Serial.print(": "); Serial.print(ErrorCodeToString(code)); Serial.print(" "); Serial.print(__FILE__); Serial.print(":"); Serial.print(__LINE__); Serial.print("\n  Message: "); Serial.println(msg);)
+  (fmt::print(fmt::fg(fmt::color::red), "ALTRO ERROR Code {}: {} {}:{}\n  Message: {}\n", (static_cast<int>(code)), ErrorCodeToString(code), __FILE__, __LINE__, (msg)), code)
 #endif
 
 namespace altro {
